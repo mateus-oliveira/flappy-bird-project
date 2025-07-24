5c3296ad6b810b954105d31af6a43db8e93f86e0
Mateus Oliveira <devmateusalves@gmail.com>
2025-07-24 11:57:53 -0300

    feat: implement pipe spawning and movement mechanics

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,12 +5,23 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
     [SerializeField] private GameObject message, duck;
+    [SerializeField] private GameObject pipes, source;
+    private float interval = 1f;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnPipes", 0f, interval);
+    }
+
+    private void SpawnPipes()
+    {
+        Instantiate(
+            pipes,                      // Instancia os pilares
+            source.transform.position,  // Na posição do objeto source
+            Quaternion.identity         // Com rotação padrão (sem rotação, identidade)
+        );
     }
 
     // Update is called once per frame
@@ -22,5 +33,7 @@ public class GameController : MonoBehaviour
             Destroy(message);
             duck.SetActive(true);
         }
+
+
     }
 }
